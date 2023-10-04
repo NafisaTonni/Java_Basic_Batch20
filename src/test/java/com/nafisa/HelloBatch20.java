a38fd40262361ad59c170818a305a5737d83ecf8
NafisaTonni <nillyonni@gmail.com>
2023-10-04 16:26:32 +0600

    first commit

---
+++ src/test/java/com/nafisa/HelloBatch20.java
@@ -0,0 +1,12 @@
+package com.nafisa;
+
+public class HelloBatch20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Batch 20");
+
+	}
+
+}
